5243bba82e41088adf80b492a3c956c6849a0dd5
Adrian Ferreres <adrian.ferreres@sngular.com>
2020-02-24 12:46:37 +0100

    Check if removing the test_python the import works

--- test_lib.py
+++
@@ -1,8 +0,0 @@
-from repository.people_repository import peopleRepository
-import json
-
-peopleRepository.createPerson('Adrian', 'Ferreres', 'ardi@mail.com')
-listPeople  = peopleRepository.listPeople(0,5)
-jsonList = json.dumps(listPeople)
-
-print('People list: {}'.format(jsonList))
